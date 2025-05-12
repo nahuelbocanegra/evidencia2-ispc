339804046fd3e82edc79686b9f4860286c50833d
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-05-12 09:55:52 -0300

    menu de opciones

--- Programacion/main.py
+++ Programacion/main.py
@@ -2,10 +2,22 @@
 
 while True:
 
-    print("elija una opcion: ")
-    
     print("1 - Listar")
     print("2 - Buscar")
     print("3 - Agregar")
     print("4 - Eliminar")
     print("5 - Salir ")
+
+    opcion = int(input("Elija una opcion:   "))
+    
+
+    if opcion == 1:
+        pass
+    if opcion == 2:
+        pass
+    if opcion == 3:
+        pass
+    if opcion == 4:
+        pass
+    if opcion == 5:
+        break
