20cafc404b3c96873676f61ffbfddf4d79f3051e
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-05-12 09:32:33 -0300

    creacion del main.py

---
+++ Programacion/main.py
@@ -0,0 +1,11 @@
+
+
+while True:
+
+    print("elija una opcion: ")
+    
+    print("1 - Listar")
+    print("2 - Buscar")
+    print("3 - Agregar")
+    print("4 - Eliminar")
+    print("5 - Salir ")
